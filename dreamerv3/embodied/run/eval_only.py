8e9c486c405e37441ce5697c52f5c6f8b0687767
Thomas Tumiel <ttumiel@gmail.com>
2023-04-30 21:55:02 +0100

    record videos in eval

--- dreamerv3/embodied/run/eval_only.py
+++ dreamerv3/embodied/run/eval_only.py
@@ -2,12 +2,17 @@ import re
 
 import embodied
 import numpy as np
+import moviepy.editor
 
 
-def eval_only(agent, env, logger, args):
+def eval_only(agent, env, logger, args, save_video=False):
 
   logdir = embodied.Path(args.logdir)
   logdir.mkdirs()
+  if save_video:
+    video_dir = logdir / "eval_videos"
+    video_dir.mkdirs()
+
   print('Logdir', logdir)
   should_log = embodied.when.Clock(args.log_every)
   step = logger.step
@@ -21,7 +26,9 @@ def eval_only(agent, env, logger, args):
   timer.wrap('logger', logger, ['write'])
 
   nonzeros = set()
+  n_videos = 0
   def per_episode(ep):
+    nonlocal n_videos
     length = len(ep['reward']) - 1
     score = float(ep['reward'].astype(np.float64).sum())
     logger.add({'length': length, 'score': score}, prefix='episode')
@@ -30,6 +37,10 @@ def eval_only(agent, env, logger, args):
     for key in args.log_keys_video:
       if key in ep:
         stats[f'policy_{key}'] = ep[key]
+        if save_video:
+          video = moviepy.editor.ImageSequenceClip(list(ep[key]), fps=20)
+          video.write_videofile(str(video_dir / f"video_{n_videos}.mp4"), logger=None)
+          n_videos += 1
     for key, value in ep.items():
       if not args.log_zeros and key not in nonzeros and (value == 0).all():
         continue
@@ -46,16 +57,15 @@ def eval_only(agent, env, logger, args):
   driver.on_episode(lambda ep, worker: per_episode(ep))
   driver.on_step(lambda tran, _: step.increment())
 
-  checkpoint = embodied.Checkpoint()
-  checkpoint.agent = agent
-  checkpoint.load(args.from_checkpoint, keys=['agent'])
+  if args.from_checkpoint:
+    checkpoint = embodied.Checkpoint()
+    checkpoint.agent = agent
+    checkpoint.load(args.from_checkpoint, keys=['agent'])
 
   print('Start evaluation loop.')
   policy = lambda *args: agent.policy(*args, mode='eval')
-  while step < args.steps:
-    driver(policy, steps=100)
-    if should_log(step):
-      logger.add(metrics.result())
-      logger.add(timer.stats(), prefix='timer')
-      logger.write(fps=True)
+  driver(policy, episodes=args.eval_eps)
+  logger.add(metrics.result())
+  logger.add(timer.stats(), prefix='timer')
+  logger.write(fps=True)
   logger.write()
