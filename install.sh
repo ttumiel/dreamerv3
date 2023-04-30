cad64b7977d671b19a6de002a7e97c0946fdf174
Thomas Tumiel <ttumiel@gmail.com>
2023-04-30 21:52:34 +0100

    install

---
+++ install.sh
@@ -0,0 +1,13 @@
+set -ex
+
+pip install --upgrade pip
+
+# CUDA 12 installation
+# Note: wheels only available on linux.
+# pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+
+# CUDA 11 installation
+# Note: wheels only available on linux.
+pip install --upgrade "jax[cuda11_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+
+pip install -e .
